f587f149259f082ec9501730f1c942eb499f288e
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-08-01 14:05:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,16 @@
 function stringChop(str, size) {
-  // your code here
-let  n=size;
-let n_cuur=n;
-let init=0;
-let strf="[";
-while(true)
-{
-    if(n_cuur>str.length)
-    {
-        strf+='"'
-        strf+=(str.substring(init));
-        strf+='"]'
-        break;
-    }
-    else{
- strf+='"'
-strf+=(str.substring(init,n_cuur));
-strf+='", '
-		str=(str.substring(init,n_cuur);
-
-    }
-}
-str=strf;
-return(str);
+const result = [];
 	
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.substring(i, i + size));
+  }
+	
+  let fnal=JSON.stringify(result);
+	return(fnal)
 }
+
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size."); 
-alert(stringChop(str, size));
+ const str = prompt("Enter String.");
+ const size = prompt("Enter Chunk Size.");
+ alert(stringChop(str, size));
+
