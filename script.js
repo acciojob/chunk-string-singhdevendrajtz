8f1f31e475155e9c0702f9ed9691aabe6b9019f3
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-08-01 14:46:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function stringChop(str, size) {
 const result = []; 
-	
+	if(str.length!=0)
+	{
   while(true)
-  {
+  {  
     if(size<str.length)
     {
       result.push(str.substring(0,size));
@@ -16,13 +17,14 @@ const result = [];
     }
     
   }
+	}
 //let pro=JSON.stringify(result);
 return(result);
 }
 
 // Do not change the code below
- const str = prompt("Enter String.");
- const size = prompt("Enter Chunk Size.");
- alert(stringChop(str, size));
+ // const str = prompt("Enter String.");
+ // const size = prompt("Enter Chunk Size.");
+ // alert(stringChop(str, size));
 
 
